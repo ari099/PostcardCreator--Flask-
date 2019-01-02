d951d6034c117e9e5bf4c949010b6bfe52884289
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-01-02 10:45:34 -0500

    Finally got the AJAX part working but the DELETE request still gives a 405 error

--- static/site.js
+++ static/site.js
@@ -1,13 +1,16 @@
-$("#del1").click(function(e) {
-   e.preventDefault();
-   $.ajax({
-      type: "DELETE",
-      url: "/DeletePhoto/1",
-      success: function(result) {
-         location.href = "/Postcards";
-      },
-      error: function(result) {
-         location.href = "/Postcards";
-      }
+$(document).ready(function() {
+   $("#del1").click(function(e) {
+      e.preventDefault();
+      $.ajax({
+         type: "DELETE",
+         url: "/DeletePhoto/1",
+         success: function(result) {
+            location.href = "/Postcards";
+         },
+         error: function(result) {
+            location.href = "/Postcards";
+            // alert(result);
+         }
+      });
    });
 });
